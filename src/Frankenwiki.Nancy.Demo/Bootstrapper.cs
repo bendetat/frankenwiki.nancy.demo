5f39235791e31e1be7cb35efe438a79048e5f004
Ben Scott <ben@belfryimages.com.au>
2015-06-26 10:37:58 +1000

    new nancy project

---
+++ src/Frankenwiki.Nancy.Demo/Bootstrapper.cs
@@ -0,0 +1,8 @@
+﻿using Nancy;
+
+namespace Frankenwiki.Nancy.Demo
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+    }
+}
---
+++ src/Frankenwiki.Nancy.Demo/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace Frankenwiki.Nancy.Demo
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
